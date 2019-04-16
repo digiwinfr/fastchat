841c9c45cb417403393458a831859ef1ff5a0c9a
François LEPORCQ <francois.leporcq@trsb.net>
2019-04-16 14:19:05 +0200

    chore(composer): Bootstrapping

---
+++ config/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+define('ROOT', dirname(__FILE__, 2));
+require_once ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
--- www/index.php
+++ www/index.php
@@ -1,2 +1,3 @@
 <?php
-echo phpinfo();
+define('ROOT', dirname(__FILE__, 2));
+require_once ROOT . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'bootstrap.php';
